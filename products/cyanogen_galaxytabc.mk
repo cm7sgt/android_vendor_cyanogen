a125c530a0b6f9fff2afe2c1b688f9136d487203
technomancer <dominikbehr@yahoo.com>
2011-10-20 23:06:20 -0700

    fix modversion to use new variables
    Change-Id: I2df2ef0bb4303c0b4a61be5703b0a780e93df00f

--- products/cyanogen_galaxytabc.mk
+++ products/cyanogen_galaxytabc.mk
@@ -35,27 +35,10 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/galaxytabc
 #PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=p1_cm7_defconfig
 #PRODUCT_SPECIFIC_DEFINES += TARGETARCH=arm
 
-# Add additional mounts
-#PRODUCT_PROPERTY_OVERRIDES += \
-#    ro.removablemounts=/mnt/emmc
-
-#
-# Set ro.modversion
-#
-ifdef CYANOGEN_NIGHTLY
-    PRODUCT_PROPERTY_OVERRIDES += \
-        ro.modversion=CyanogenMod-7--NIGHTLY-GalaxyTab
-else
-    ifdef CYANOGEN_RELEASE
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7.1.0-Beta-GalaxyTab
-    else
-#        PRODUCT_PROPERTY_OVERRIDES += \
-#            ro.modversion=CyanogenMod-7.1.0-Beta-GalaxyTab-KANG
-        PRODUCT_PROPERTY_OVERRIDES += \
-            ro.modversion=CyanogenMod-7-$(shell date +%Y-%m-%d)-Beta-GalaxyTab-KANG
-    endif
-endif
+# Release name and versioning
+PRODUCT_RELEASE_NAME := GalaxyTabC
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+-include vendor/cyanogen/products/common_versions.mk
 
 #
 # Copy galaxytab specific prebuilt files
